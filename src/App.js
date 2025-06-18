960a858de111c5a5cea6de004cfd040cf488f760
Yorick MOUSSAVOU <yorickmoussavou15@gmail.com>
2025-06-19 00:04:42 +0100

    Commit simple

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 export default function App (props){
   const greenting = props.greeting
   return (
-    <h1>Hello World, de la part de {greenting} </h1>
-  )
+    <>
+      <h1>Hello World, de la part de {greenting} </h1>
+      <p>Je vous présente mon site qui ressemble un peu à un portefolio </p>
+    </>
+  );
 }
